cea55b4709b042f442e7907435f15c731115679a
compc4 <compc4@gmx.com>
2017-05-19 22:52:32 +0900

    Create Batuino-BT_cfg.ino

---
+++ Batuino-BT/Batuino-BT_cfg.ino
@@ -0,0 +1,44 @@
+#define TIMEOUT 5000 // mS
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  
+  delay(3000);
+  Serial.begin(38400);
+  SendCommand("AT", "OK");
+  SendCommand("AT+NAME=Batuino-BT", "OK");
+  //SendCommand("AT+ROLE=1", "OK");  //master mode
+  SendCommand("AT+ROLE=0", "OK");  //slave mode
+  SendCommand("AT+UART=9600,0,0", "OK");  //Baud rate 9600, stop bit 1, parity bit 0
+  SendCommand("AT+PSWD=1234", "OK");  //Passkey 1234
+  SendCommand("AT+POLAR=1,0", "OK");  //PI09 output low level (for uploading Sketch via Bluetooth)
+  digitalWrite(13, HIGH);
+}
+
+void loop() {
+  
+}
+
+boolean SendCommand(String cmd, String ack){
+  Serial.println(cmd); // Send "AT+" command to module
+  if (!echoFind(ack, TIMEOUT)) // timed out waiting for ack string
+    return true; // ack blank
+    else return false; //  ack found
+}
+ 
+boolean echoFind(String keyword, int timeout){
+ byte current_char = 0;
+ byte keyword_length = keyword.length();
+ long deadline = millis() + timeout;
+ while(millis() < deadline){
+  if (Serial.available()){
+    char ch = Serial.read();
+    if (ch == keyword[current_char])
+      if (++current_char == keyword_length){
+       return true;
+    }
+   }
+  }
+ return false; // Timed out
+}
